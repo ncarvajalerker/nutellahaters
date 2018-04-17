e0ec36c3718afce7889cd8d8fad3542ba56bd4cb
ncarvajalerker <36217235+ncarvajalerker@users.noreply.github.com>
2018-04-17 10:38:04 -0400

    Update csim.c

--- csim.c
+++ csim.c
@@ -7,7 +7,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void printUsage();
+
+void clear_cache(){ //cache, size
+	if( != NULL){
+		//clear cache sets
+	}	
+}	
+
+void printUsage(char * argv[]) {
+	printf("./csim-ref: Missing required command line argument\n");
+	printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
+	printf("Options:\n");
+	printf("  -h         Print this help message.");
+	printf("  -v         Optional verbose flag.\n");
+	printf("  -s <num>   Number of set index bits.\n");
+	printf("  -E <num>   Number of lines per set.\n");
+	printf("  -b <num>   Number of block offset bits.\n");
+	printf("  -t <file>  Trace file.\n\n");
+	printf("Examples:\n");
+	printf("  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n");
+	printf("  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace");
+}
 
 int main(int argc, char* argv[])
 {
@@ -27,6 +47,7 @@ int main(int argc, char* argv[])
 	char *line = NULL;
 	size_t len = 0;
 	ssize_t read;
+	int size;
 
 	fp = fopen(filename, 'r');
 	if (fp == NULL) {
@@ -59,22 +80,8 @@ int main(int argc, char* argv[])
 	fclose(fp);
 	if (line)
 		free(line);
-    printSummary(0, 0, 0);
+    printSummary(0, 0, 0); //hits, misses, evicts
+    clear_cache();
 
     return EXIT_SUCCESS;
-}
-
-void printUsage() {
-	printf("./csim-ref: Missing required command line argument\n");
-	printf("Usage: ./csim-ref [-hv] -s <num> -E <num> -b <num> -t <file>\n");
-	printf("Options:\n");
-	printf("  -h         Print this help message.");
-	printf("  -v         Optional verbose flag.\n");
-	printf("  -s <num>   Number of set index bits.\n");
-	printf("  -E <num>   Number of lines per set.\n");
-	printf("  -b <num>   Number of block offset bits.\n");
-	printf("  -t <file>  Trace file.\n\n");
-	printf("Examples:\n");
-	printf("  linux>  ./csim-ref -s 4 -E 1 -b 4 -t traces/yi.trace\n");
-	printf("  linux>  ./csim-ref -v -s 8 -E 2 -b 4 -t traces/yi.trace");
 }
